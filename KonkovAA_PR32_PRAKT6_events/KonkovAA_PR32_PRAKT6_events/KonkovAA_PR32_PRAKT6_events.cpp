625ea744af2ff58717f93d39d96dab5fd29a8951
Ghosterenk0 <arsenijkonkov912@gmail.com>
2025-12-02 16:09:44 +0500

    Update KonkovAA_PR32_PRAKT6_events.cpp

--- KonkovAA_PR32_PRAKT6_events/KonkovAA_PR32_PRAKT6_events/KonkovAA_PR32_PRAKT6_events.cpp
+++ KonkovAA_PR32_PRAKT6_events/KonkovAA_PR32_PRAKT6_events/KonkovAA_PR32_PRAKT6_events.cpp
@@ -1,32 +1,61 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
+class HandlersList {
+    struct Node {
+    public:
+        void (*handler)(const string&);
+        Node* next;
+        Node* prev;
+
+        Node(void(*Handler)(const string&)) : handler(Handler), next(nullptr), prev(nullptr) {}
+    };
+    Node* head;
+    Node* tail;
+
+public:
+    void push_back(void(*Handler)(const string&)) {
+        Node* node = new Node(Handler);
+        if (tail == nullptr) {
+            head = node;
+            tail = node;
+        }
+        else {
+            node->prev = tail;
+            tail->next = node;
+            tail = node;
+        }
+    }
+    void display(const string &mes) {
+        Node* node = head;
+        while (node != nullptr) {
+            node->handler(mes);
+            node = node->next;
+        }
+    }
+
+};
 struct EventSystem {
-    void (*handlers[100])(const string&);
-    int count = 0;
+    HandlersList hl;
 };
 
-void registerHandler(EventSystem& system, void(*handlers)(const string &message)) {
-    system.handlers[system.count] = handlers;
-    system.count++;
-}
 
-void triggerEvent(const EventSystem &system, const string &message) {
-    for (int i = 0; i < system.count; i++)
-    {
-        system.handlers[i](message);
-    }
+void registerHandler(EventSystem& system, void(*handlers)(const string& message)) {
+    system.hl.push_back(handlers);
 }
-
-void onUserLogin(const string &message) {
+void triggerEvent(EventSystem& system, const string& message) {
+    system.hl.display(message);
+}
+void onUserLogin(const string& message) {
     cout << "Пользователь вошел в систему: " << message << endl;
 }
-void onUserLogout(const string &message) {
+void onUserLogout(const string& message) {
     cout << "Пользователь вышел из системы: " << message << endl;
 }
-void onError(const string &message) {
+void onError(const string& message) {
     cout << "Произошла ошибка: " << message << endl;
 }
+
 
 int main()
 {
@@ -40,4 +69,3 @@ int main()
     triggerEvent(sys, "User");
     triggerEvent(sys, "Error");
 }
-
